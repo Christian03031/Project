b12dcf02bd41d0fc607f5a8532b655d7374d4b35
Кристиан Амежее <amegee.c.christian@gmail.com>
2026-09-25 11:11:18 +0300

    adding text message in case basket is empty

--- src/components/basket/Basket.jsx
+++ src/components/basket/Basket.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router";
 import { useBasket, useDispatch } from "../../context/contextBasket";
 import "./basket.css"
 
+import { FaceSlightlyFrowning } from "lucide-react";
+
 export function Basket() {
 
     const basket = useBasket();
@@ -11,75 +13,90 @@ export function Basket() {
 
     const navigate = useNavigate();
 
-    return basket.length > 0 && <div className="basket-container">
-        <table className="basket-table">
-            <colgroup>
-                <col className="dish-col" />
-                <col />
-                <col />
-                <col />
-                <col />
-            </colgroup>
-            <thead>
-                <tr>
-                    <td>Dish</td>
-                    <td>Price</td>
-                    <td>Quantity</td>
-                    <td>Total</td>
-                    <td>Action</td>
-                </tr>
-            </thead>
-
-            <tbody>
 
 
-                {
-                    basket.map(a => <tr key={a.id}>
-                        <td><div><img src={a.img} alt="dish-img" /><span>{a.name}</span></div></td>
-                        <td>{a.price}$</td>
-                        <td>
-                            {a.quantity === 1 && <span className="action minus"> </span>}
-                            {a.quantity > 1 && <button className="action minus" onClick={
-                                e => {
-                                    dispatch({ type: 'REMOVE_DUPLICATE_BASKET', payload: { ...a } });
-                                }}>-</button>}
-                            {a.quantity}
-                            <button className="action add" onClick={
-                                e => {
-                                    dispatch({ type: 'DUPLICATE_BASKET', payload: { ...a } });
-                                }}>+</button></td>
-                        <td>{a.price * a.quantity}$</td>
-                        <td style={{
-                            flexDirection: "row",
-                            alignItems: "end",
-                            justifyContent: "center",
-                        }}><button className="action delete" onClick={
-                            e => {
-                                dispatch({ type: 'REMOVE_FROM_BASKET', payload: { ...a } });
-                            }}>x</button>
 
-                        </td></tr>)
-                }
-            </tbody>
-        </table>
-
-        <div className="total-container">
-            <div className="receipt">
-                <h2>Card Totals</h2>
-                <table className="receipt-table">
-                    <tbody>
-                        <tr><td>Subtotal</td><td>{total}$</td></tr>
-                        <tr><td>Delivery Fee</td><td>{deliveryFee}$</td></tr>
-                        <tr><td>Total</td><td>{total + deliveryFee}$</td></tr>
-                    </tbody>
-                </table>
-                <button className="checkout" onClick={() => { navigate("/order") }}>PROCEED TO CHECKOUT</button>
-            </div>
-            <div className="promocode">
-                <span>If you have a promocode, enter it here</span>
-                <div className="input-container"><input type="text" placeholder="promo code" /><button>Submit</button></div>
-            </div>
+    return basket.length === 0 && <div className="basket-container-empty">
+        <div className="label-empty">
+            <FaceSlightlyFrowning size={32} />
+            <h1>Empty</h1>
+        </div>
+        <div className="label-content">
+            <p>Your basket is empty right now, try to find something in the menu and add order it !</p>
+            <button onClick={() => { navigate("/menu") }}>To menu</button>
         </div>
 
-    </div >
+    </div> ||
+
+        basket.length > 0 && <div className="basket-container">
+            <table className="basket-table">
+                <colgroup>
+                    <col className="dish-col" />
+                    <col />
+                    <col />
+                    <col />
+                    <col />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <td>Dish</td>
+                        <td>Price</td>
+                        <td>Quantity</td>
+                        <td>Total</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+
+                    {
+                        basket.map(a => <tr key={a.id}>
+                            <td><div><img src={a.img} alt="dish-img" /><span>{a.name}</span></div></td>
+                            <td>{a.price}$</td>
+                            <td>
+                                {a.quantity === 1 && <span className="action minus"> </span>}
+                                {a.quantity > 1 && <button className="action minus" onClick={
+                                    e => {
+                                        dispatch({ type: 'REMOVE_DUPLICATE_BASKET', payload: { ...a } });
+                                    }}>-</button>}
+                                {a.quantity}
+                                <button className="action add" onClick={
+                                    e => {
+                                        dispatch({ type: 'DUPLICATE_BASKET', payload: { ...a } });
+                                    }}>+</button></td>
+                            <td>{a.price * a.quantity}$</td>
+                            <td style={{
+                                flexDirection: "row",
+                                alignItems: "end",
+                                justifyContent: "center",
+                            }}><button className="action delete" onClick={
+                                e => {
+                                    dispatch({ type: 'REMOVE_FROM_BASKET', payload: { ...a } });
+                                }}>x</button>
+
+                            </td></tr>)
+                    }
+                </tbody>
+            </table>
+
+            <div className="total-container">
+                <div className="receipt">
+                    <h2>Card Totals</h2>
+                    <table className="receipt-table">
+                        <tbody>
+                            <tr><td>Subtotal</td><td>{total}$</td></tr>
+                            <tr><td>Delivery Fee</td><td>{deliveryFee}$</td></tr>
+                            <tr><td>Total</td><td>{total + deliveryFee}$</td></tr>
+                        </tbody>
+                    </table>
+                    <button className="checkout" onClick={() => { navigate("/order") }}>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="promocode">
+                    <span>If you have a promocode, enter it here</span>
+                    <div className="input-container"><input type="text" placeholder="promo code" /><button>Submit</button></div>
+                </div>
+            </div>
+
+        </div >
 }
